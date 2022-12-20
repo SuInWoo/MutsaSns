bd46e4055d5605f6672bc4176572b13733f31df0
SuInWoo <wbo1026@naver.com>
2022-12-20 13:51:26 +0900

    ADD TestController 1. TestController

--- src/main/java/com/sns/controller/TestController.java
+++ src/main/java/com/sns/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sns.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello");
     }
 }
