996c7650ca292df54ffd9a9b0c3b2bef75ff85b4
SuInWoo <wbo1026@naver.com>
2022-12-21 09:39:09 +0900

    Fix HelloController

--- src/main/java/com/sns/controller/HelloController.java
+++ src/main/java/com/sns/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-public class TestController {
+public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
