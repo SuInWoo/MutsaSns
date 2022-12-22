1e8910ad0637cf9077bbd14a9a51e34d3ca0bb86
SuInWoo <wbo1026@naver.com>
2022-12-22 09:06:59 +0900

    Fix darkchocolate

--- src/main/java/com/sns/controller/HelloController.java
+++ src/main/java/com/sns/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("hello");
+        return ResponseEntity.ok().body("darkchocolate");
     }
 }
