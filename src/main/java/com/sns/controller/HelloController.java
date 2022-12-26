5b7d5ce069a11c90e5d152d6b6f8ba684fc1cd0d
SuInWoo <wbo1026@naver.com>
2022-12-26 09:02:06 +0900

    Feat Keyword - happy_new_year

--- src/main/java/com/sns/controller/HelloController.java
+++ src/main/java/com/sns/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("darkchocolate");
+        return ResponseEntity.ok().body("happy_new_year");
     }
 }
