72b98794115251e26b71bea2e3552d2df9ccf05e
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 <wbo1026@naver.com>
2022-12-27 15:28:53 +0900

    Feat PostControllerTest 1. 삭제 성공 2. 삭제 실패 - 인증 실패, 작성자 불일치, DB 에러

--- src/test/java/com/sns/controller/PostControllerTest.java
+++ src/test/java/com/sns/controller/PostControllerTest.java
@@ -2,6 +2,7 @@ package com.sns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sns.domain.dto.post.PostCreateReq;
+import com.sns.domain.dto.post.PostDeleteReq;
 import com.sns.domain.dto.post.PostDto;
 import com.sns.domain.dto.post.PostUpdateReq;
 import com.sns.exception.AppException;
@@ -23,6 +24,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -255,5 +257,83 @@ class PostControllerTest {
           -- (DB 에러)
      */
 
+    @Test
+    @WithMockUser
+    void 삭제성공() throws Exception {
+        PostDeleteReq req = PostDeleteReq.builder()
+                .postId(1L)
+                .build();
+
+        PostDto postDto = PostDto.builder()
+                .id(1L)
+                .build();
+
+        when(postService.delete(any(), any()))
+                .thenReturn(postDto);
+
+        mockMvc.perform(delete(String.format("/api/v1/posts/%s", postDto.getId()))
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(req)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+                .andExpect(jsonPath("$.result.postId").value(1L))
+                .andExpect(jsonPath("$.result.message").value("포스트 삭제 완료"));
+
+    }
+
+    @Test
+    @WithMockUser
+    void 삭제실패_인증실패() throws Exception {
+
+        when(postService.delete(any(), any()))
+                .thenThrow(new AppException(ErrorCode.INVALID_TOKEN, ErrorCode.INVALID_TOKEN.getMessage()));
+
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()))
+                .andExpect(jsonPath("$.resultCode").value("ERROR"))
+                .andExpect(jsonPath("$.result.errorCode").value("INVALID_TOKEN"))
+                .andExpect(jsonPath("$.result.message").value("잘못된 토큰입니다."));;
+
+    }
+
+    @Test
+    @WithMockUser
+    void 삭제실패_작성자불일치() throws Exception {
+
+        when(postService.delete(any(), any()))
+                .thenThrow(new AppException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage()));
+
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()))
+                .andExpect(jsonPath("$.resultCode").value("ERROR"))
+                .andExpect(jsonPath("$.result.errorCode").value("INVALID_PERMISSION"))
+                .andExpect(jsonPath("$.result.message").value("사용자가 권한이 없습니다."));;
+
+    }
+
+    @Test
+    @WithMockUser
+    void 삭제실패_DB에러() throws Exception {
+
+        when(postService.delete(any(), any()))
+                .thenThrow(new AppException(ErrorCode.DATABASE_ERROR, ErrorCode.DATABASE_ERROR.getMessage()));
+
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.DATABASE_ERROR.getStatus().value()))
+                .andExpect(jsonPath("$.resultCode").value("ERROR"))
+                .andExpect(jsonPath("$.result.errorCode").value("DATABASE_ERROR"))
+                .andExpect(jsonPath("$.result.message").value("DB에러"));;
+    }
 
 }
